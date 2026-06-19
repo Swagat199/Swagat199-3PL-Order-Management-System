512997d8fa0bb790ca10a289e00fe8dcf040c5ab
Swagat Pattanayak <pattanayakswagat2004@gmail.com>
2026-06-20 02:08:45 +0530

    Enhance search functionality with clear input option and improve styling

--- frontend/javascript/viewOrders.js
+++ frontend/javascript/viewOrders.js
@@ -1,4 +1,9 @@
 const BASE_URL = "http://localhost:5000/api/orders";
+const searchInput =
+  document.getElementById("searchInput");
+
+const clearSearch =
+  document.getElementById("clearSearch");
 
 async function loadOrders() {
     const response = await fetch(BASE_URL);
@@ -19,11 +24,11 @@ function displayOrders(data) {
             <td>${order.quantity}</td>
             <td>${order.status}</td>
             <td class='actions'>
-                <button onclick="editOrder(${order.order_id})">
+                <button onclick="editOrder(${order.order_id})" id="editBtn">
                     Edit
                 </button>
 
-                <button onclick="deleteOrder(${order.order_id})">
+                <button onclick="deleteOrder(${order.order_id})" id="deleteBtn">
                     Delete
                 </button>
             </td>
@@ -43,9 +48,9 @@ async function searchOrders() {
     const data = await response.json();
 
     const filtered = data.filter(order =>
-        order.product_name
-            .toLowerCase()
-            .includes(searchText)
+        order.product_name.toLowerCase().includes(searchText) ||
+        order.company_name.toLowerCase().includes(searchText) ||
+        order.status.toLowerCase().includes(searchText)
     );
 
     displayOrders(filtered);
@@ -70,6 +75,22 @@ async function deleteOrder(id) {
 function editOrder(id) {
     window.location.href =
         `update-order.html?id=${id}`;
+}
+
+searchInput.addEventListener("input", ()=>{
+     if (searchInput.value.trim() !== "") {
+    clearSearch.style.display = "block";
+  } else {
+    clearSearch.style.display = "none";
+  }
+},
+);
+
+function clearSearchInput() {
+  searchInput.value = "";
+  clearSearch.style.display = "none";
+
+  loadOrders();
 }
 
 loadOrders();
